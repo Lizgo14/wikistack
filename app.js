b29205b0c5cfb02c0af8bdb45f9c5800d8832d4c
Laura Nemeth <lauranemeth.pr@gmail.com>
2019-11-14 11:51:12 -0500

    added empty string

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
 const app = express();
+// const style = require("./stylesheets/style")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
@@ -9,9 +10,10 @@ app.use(express.static(__dirname + "/public"))
 
 
 app.get('/', (req,res,next)=>{
-  console.log('hello world')
+  res.send("")
 })
 
-app.listen(1337, () => {
+
+app.listen(3000, () => {
   console.log(`App listening in port`);
 });
